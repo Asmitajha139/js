e135da140a9f7b0866ba7fd60d5e1b308b53533a
Asmita Jha <151938400+Asmitajha139@users.noreply.github.com>
2026-04-18 17:56:47 +0000

    dataTypes

---
+++ 01_basico/02_dataType.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all Js code as newer js version
+
+//alert(3+3) //we r using node js not browser
+
+//code readability should be high
+
+let name ="Asmita"
+let age= 18
+let isloggedIn =false
+let state; // to represent the 
+//number -> 2 to power 53
+//bigint
+//string =>
+//boolean ->true/false
+//null => standalone value
+// undefined => 
+//symbol=> unique  //we see more implementation when we will learn react
+//object
+
+console.log(typeof "Asmita"); //string
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
