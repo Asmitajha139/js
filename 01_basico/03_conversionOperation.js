ff3728618bc8a5b3f03e2ff65ec561cddd76d438
Asmita Jha <151938400+Asmitajha139@users.noreply.github.com>
2026-04-18 19:08:08 +0000

    conversionOperation

--- 01_basico/03_conversionOperation.js
+++ 01_basico/03_conversionOperation.js
@@ -22,11 +22,20 @@ let isloggedIn = 1 // if this is empty then false
 //"" =>false
 //"Asmita" =>true
 
-let booleanIsLoggedIn = Boolean(isloggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
 
-let someNumber =33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber =33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//***********************Operations ************************** */
+let value =3
+let negValue = -value
+console.log(negValue);
+
+let str3=str1 +str2
+console.log(str3)
+
 
