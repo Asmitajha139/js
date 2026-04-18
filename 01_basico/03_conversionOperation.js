1d069e93e66bbe8f6a984b1fe9c9d664f4d114ca
Asmita Jha <151938400+Asmitajha139@users.noreply.github.com>
2026-04-18 18:18:40 +0000

    conversionOfDataTypes

---
+++ 01_basico/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score ="33aa"
+
+
+
+//console.log(typeof score);
+//console.log(typeof(score));
+// const {score} =
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" =>33
+//"33abc" =>NaN
+//tue =>1; false =>0
+
+
+let isloggedIn = 1 // if this is empty then false
+
+//1=>true; 0=>false
+//"" =>false
+//"Asmita" =>true
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
